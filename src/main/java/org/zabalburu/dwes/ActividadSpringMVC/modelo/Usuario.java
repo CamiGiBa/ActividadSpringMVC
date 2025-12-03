c55f5ce390fccae6b1cf5e90d49592fa918c3137
Camilo <camilo.giraldo.baena@zabalburu.org>
2025-12-03 09:34:52 +0100

    commit clases

--- src/main/java/org/zabalburu/dwes/ActividadSpringMVC/modelo/Usuario.java
+++ src/main/java/org/zabalburu/dwes/ActividadSpringMVC/modelo/Usuario.java
@@ -1,5 +1,43 @@
 package org.zabalburu.dwes.ActividadSpringMVC.modelo;
 
-public class Usuario {
+import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@Table(name = "usuarioActividad")
+@EqualsAndHashCode(onlyExplicitlyIncluded =  true)	
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private long id;
+	
+	@Column(nullable = false, unique = true)
+	private String email; 
+	
+	@Column(nullable = false )
+	private String password; 
+	
+	 @Column(nullable = false)
+     private String nombre;
+
+	 @OneToMany(mappedBy="usuario")
+	 private List<Pelicula> peliculas;
+
+	 @Override
+	 public String toString() {
+		return "Usuario [id=" + id + ", email=" + email + ", password=" + password + ", nombre=" + nombre + "]";
+	 }
+	 
+	 
 }
